325f2cd4415c136aa599e553406534aa9b056d7a
Andreasedgarp01 <andreasedgarp@gmail.com>
2022-04-02 13:38:56 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+
+// notes
+const notes = require("./api/notes");
+const NotesService = require("./services/postgres/NotesService");
+const NotesValidator = require("./validator/notes");
+
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
+const init = async () => {
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
